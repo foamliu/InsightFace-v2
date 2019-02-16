cd5730c51060e41810d0cc8c1a88440f55d3d7fe
unknown <foamliu@yeah.net>
2019-02-16 12:09:19 +0800

    update

---
+++ extract.py
@@ -0,0 +1,24 @@
+import mxnet as mx
+from mxnet import recordio
+import pickle
+from tqdm import tqdm
+
+from config import path_imgidx, path_imgrec
+
+if __name__ == "__main__":
+    imgrec = recordio.MXIndexedRecordIO(path_imgidx, path_imgrec, 'r')
+
+    samples = []
+
+    # %% 1 ~ 3804847
+    for i in tqdm(range(3804846)):
+        try:
+            header, s = recordio.unpack(imgrec.read_idx(i+1))
+            img = mx.image.imdecode(s).asnumpy()
+            label = int(header.label)
+            samples.append({'img': img, label: label})
+        except:
+            pass
+
+    with open('data/faces_ms1m_112x112.pickle', 'wb') as file:
+        pickle.dump(samples, file)
