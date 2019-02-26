90be9ef57cb78602ad8666eb27e3a33968e90e5f
Yang Liu <liuyang12@focusmedia.cn>
2019-02-26 09:44:30 +0800

    update

--- pre_process.py
+++ pre_process.py
@@ -1,15 +1,37 @@
 import os
+import pickle
 
-import zipfile
+import cv2 as cv
+import mxnet as mx
+from mxnet import recordio
+from tqdm import tqdm
 
-
-def extract(filename):
-    print('Extracting {}...'.format(filename))
-    zip_ref = zipfile.ZipFile(filename, 'r')
-    zip_ref.extractall('data')
-    zip_ref.close()
-
+from config import path_imgidx, path_imgrec, IMG_DIR, pickle_file
+from utils import ensure_folder
 
 if __name__ == "__main__":
-    if not os.path.isdir('data/faces_ms1m_112x112'):
-        extract('data/faces_ms1m_112x112.zip')
+    ensure_folder(IMG_DIR)
+    imgrec = recordio.MXIndexedRecordIO(path_imgidx, path_imgrec, 'r')
+
+    samples = []
+
+    # %% 1 ~ 3804847
+    for i in tqdm(range(3804846)):
+        try:
+            header, s = recordio.unpack(imgrec.read_idx(i + 1))
+            img = mx.image.imdecode(s).asnumpy()
+            img = cv.cvtColor(img, cv.COLOR_RGB2BGR)
+            label = int(header.label)
+            filename = '{}.png'.format(i)
+            samples.append({'img': filename, 'label': label})
+            filename = os.path.join(IMG_DIR, filename)
+            cv.imwrite(filename, img)
+        except KeyboardInterrupt:
+            raise
+        except:
+            pass
+
+    with open(pickle_file, 'wb') as file:
+        pickle.dump(samples, file)
+
+    print('num_samples: ' + str(len(samples)))
