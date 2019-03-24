fdf270ed57104673c8b6e65b518215c00007b4e0
unknown <foamliu@yeah.net>
2019-03-24 10:22:44 +0800

    update

--- data_gen.py
+++ data_gen.py
@@ -4,20 +4,18 @@ import random
 from io import BytesIO
 
 import cv2 as cv
-import torch
 from PIL import Image
 from torch.utils.data import Dataset
 from torchvision import transforms
 
 from config import IMG_DIR
-from config import num_workers, pickle_file
+from config import pickle_file
 
 # Data augmentation and normalization for training
 # Just normalization for validation
 data_transforms = {
     'train': transforms.Compose([
         transforms.RandomHorizontalFlip(),
-        transforms.ColorJitter(brightness=0.125, contrast=0.125, saturation=0.125, hue=0),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
     ]),
@@ -25,18 +23,28 @@ data_transforms = {
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
+    'train-enhanced': transforms.Compose([
+        transforms.RandomHorizontalFlip(),
+        transforms.ColorJitter(brightness=0.125, contrast=0.125, saturation=0.125, hue=0),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+    ]),
 }
 
 
 class ArcFaceDataset(Dataset):
-    def __init__(self, split):
+    def __init__(self, args, split):
         with open(pickle_file, 'rb') as file:
             data = pickle.load(file)
 
+        self.args = args
         self.samples = data
 
         if split == 'train':
-            self.transformer = data_transforms['train']
+            if not self.args.enhanced_aug:
+                self.transformer = data_transforms['train']
+            else:
+                self.transformer = data_transforms['train-enhanced']
 
     def __getitem__(self, i):
         sample = self.samples[i]
@@ -47,7 +55,8 @@ class ArcFaceDataset(Dataset):
         img = cv.imread(filename)  # BGR
         img = img[..., ::-1]  # RGB
         img = Image.fromarray(img, 'RGB')  # RGB
-        img = self.compress_aug(img)  # RGB
+        if self.args.enhanced_aug:
+            img = self.compress_aug(img)  # RGB
         img = self.transformer(img)  # RGB
 
         return img, label
@@ -62,13 +71,3 @@ class ArcFaceDataset(Dataset):
         buf = buf.getvalue()
         img = Image.open(BytesIO(buf))
         return img
-
-
-if __name__ == "__main__":
-    train_dataset = ArcFaceDataset('train')
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=256, shuffle=True,
-                                               num_workers=num_workers,
-                                               pin_memory=True)
-
-    print(len(train_dataset))
-    print(len(train_loader))
--- utils.py
+++ utils.py
@@ -189,6 +189,7 @@ def parse_args():
     parser.add_argument('--gamma', type=float, default=2.0, help='focusing parameter gamma')
     parser.add_argument('--use-se', type=bool, default=False, help='use SEBlock')
     parser.add_argument('--full-log', type=bool, default=False, help='full logging')
+    parser.add_argument('--enhanced-aug', type=bool, default=False, help='enhanced data aug')
     parser.add_argument('--checkpoint', type=str, default=None, help='checkpoint')
     args = parser.parse_args()
     return args
