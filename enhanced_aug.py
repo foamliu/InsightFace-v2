4c499cfca55abe68e2cd6a1fc9854e5d600ea38f
Yang Liu <liuyang12@focusmedia.cn>
2019-03-25 16:03:15 +0800

    update

--- enhanced_aug.py
+++ enhanced_aug.py
@@ -44,53 +44,53 @@ seq = iaa.Sequential(
         # image. Don't execute all of them, as that would often be way too
         # strong.
         #
-        iaa.SomeOf((0, 5),
-                   [
-                       # Blur each image with varying strength using
-                       # gaussian blur (sigma between 0 and 3.0),
-                       # average/uniform blur (kernel size between 2x2 and 7x7)
-                       # median blur (kernel size between 3x3 and 11x11).
-                       iaa.OneOf([
-                           iaa.GaussianBlur((0, 0.50)),
-                           iaa.AverageBlur(k=(2, 3)),
-                           iaa.MedianBlur(k=(3, 5)),
-                       ]),
-
-                       # Sharpen each image, overlay the result with the original
-                       # image using an alpha between 0 (no sharpening) and 1
-                       # (full sharpening effect).
-                       iaa.Sharpen(alpha=(0, 1.0), lightness=(0.75, 1.5)),
-
-                       # Add gaussian noise to some images.
-                       # In 50% of these cases, the noise is randomly sampled per
-                       # channel and pixel.
-                       # In the other 50% of all cases it is sampled once per
-                       # pixel (i.e. brightness change).
-                       # iaa.AdditiveGaussianNoise(
-                       #     loc=0, scale=(0.0, 0.03 * 255), per_channel=0.5
-                       # ),
-
-                       # Either drop randomly 1 to 10% of all pixels (i.e. set
-                       # them to black) or drop them on an image with 2-5% percent
-                       # of the original size, leading to large dropped
-                       # rectangles.
-                       iaa.OneOf([
-                           iaa.Dropout((0.01, 0.05), per_channel=0.5),
-                           iaa.CoarseDropout(
-                               (0.03, 0.15), size_percent=(0.02, 0.05),
-                               per_channel=0.2
-                           ),
-                       ]),
-
-                       # Invert each image's chanell with 5% probability.
-                       # This sets each pixel value v to 255-v.
-                       iaa.Invert(0.05, per_channel=True),  # invert color channels
-
-                       # Add a value of -10 to 10 to each pixel.
-                       iaa.Add((-10, 10), per_channel=0.5),
-
-                       # Change brightness of images (50-150% of original value).
-                       iaa.Multiply((0.5, 1.5), per_channel=0.5),
+        # iaa.SomeOf((0, 5),
+        #            [
+        #                # Blur each image with varying strength using
+        #                # gaussian blur (sigma between 0 and 3.0),
+        #                # average/uniform blur (kernel size between 2x2 and 7x7)
+        #                # median blur (kernel size between 3x3 and 11x11).
+        #                iaa.OneOf([
+        #                    iaa.GaussianBlur((0, 0.50)),
+        #                    iaa.AverageBlur(k=(2, 3)),
+        #                    iaa.MedianBlur(k=(3, 5)),
+        #                ]),
+        #
+        #                # Sharpen each image, overlay the result with the original
+        #                # image using an alpha between 0 (no sharpening) and 1
+        #                # (full sharpening effect).
+        #                iaa.Sharpen(alpha=(0, 1.0), lightness=(0.75, 1.5)),
+        #
+        #                # Add gaussian noise to some images.
+        #                # In 50% of these cases, the noise is randomly sampled per
+        #                # channel and pixel.
+        #                # In the other 50% of all cases it is sampled once per
+        #                # pixel (i.e. brightness change).
+        #                # iaa.AdditiveGaussianNoise(
+        #                #     loc=0, scale=(0.0, 0.03 * 255), per_channel=0.5
+        #                # ),
+        #
+        #                # Either drop randomly 1 to 10% of all pixels (i.e. set
+        #                # them to black) or drop them on an image with 2-5% percent
+        #                # of the original size, leading to large dropped
+        #                # rectangles.
+        #                iaa.OneOf([
+        #                    iaa.Dropout((0.01, 0.05), per_channel=0.5),
+        #                    iaa.CoarseDropout(
+        #                        (0.03, 0.15), size_percent=(0.02, 0.05),
+        #                        per_channel=0.2
+        #                    ),
+        #                ]),
+        #
+        #                # Invert each image's chanell with 5% probability.
+        #                # This sets each pixel value v to 255-v.
+        #                iaa.Invert(0.05, per_channel=True),  # invert color channels
+        #
+        #                # Add a value of -10 to 10 to each pixel.
+        #                iaa.Add((-10, 10), per_channel=0.5),
+        #
+        #                # Change brightness of images (50-150% of original value).
+        #                iaa.Multiply((0.5, 1.5), per_channel=0.5),
 
                    ],
                    # do all of the above augmentations in random order
