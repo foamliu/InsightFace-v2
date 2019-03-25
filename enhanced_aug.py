e7ee9bc0e132c78a030e019ff7c746ca7411f269
Yang Liu <liuyang12@focusmedia.cn>
2019-03-25 10:58:28 +0800

    update

---
+++ enhanced_aug.py
@@ -0,0 +1,42 @@
+import os
+import pickle
+import random
+
+import cv2 as cv
+import numpy as np
+from PIL import Image
+from torchvision import transforms
+
+from config import IMG_DIR
+from config import pickle_file
+
+# Data augmentation and normalization for training
+# Just normalization for validation
+data_transforms = {
+    'train-enhanced': transforms.Compose([
+        transforms.RandomHorizontalFlip(),
+        transforms.ColorJitter(brightness=0.125, contrast=0.125, saturation=0.125, hue=0),
+        # transforms.ToTensor(),
+        # transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+    ]),
+}
+
+if __name__ == "__main__":
+    with open(pickle_file, 'rb') as file:
+        data = pickle.load(file)
+
+    samples = data
+    sample = random.sample(samples, 1)
+    filename = sample['img']
+    filename = os.path.join(IMG_DIR, filename)
+    transformer = data_transforms['train-enhanced']
+    filename = os.path.join(IMG_DIR, filename)
+    img = cv.imread(filename)  # BGR
+    img = img[..., ::-1]  # RGB
+    img = Image.fromarray(img, 'RGB')  # RGB
+    # img = compress_aug(img)  # RGB
+    img = transformer(img)  # RGB
+    img = np.array(img)
+    img = img[..., ::-1]  # BGR
+    cv.imshow('image', img)
+    cv.waitKey(0)
