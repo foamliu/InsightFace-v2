6548b9ed9d7c81fde340461a3c919754105b7bc4
Yang Liu <liuyang12@focusmedia.cn>
2019-09-24 15:10:46 +0800

    udpate

--- image_aug.py
+++ image_aug.py
@@ -1,15 +1,7 @@
-import os
-import pickle
-import random
-
 import cv2 as cv
 import numpy as np
-from PIL import Image
 from imgaug import augmenters as iaa
 from torchvision import transforms
-
-from config import IMG_DIR
-from config import pickle_file
 
 # Data augmentation and normalization for training
 # Just normalization for validation
@@ -35,19 +27,9 @@ def image_aug(src):
 
 
 if __name__ == "__main__":
-    with open(pickle_file, 'rb') as file:
-        data = pickle.load(file)
-
-    samples = data
-    sample = random.sample(samples, 1)[0]
-    filename = sample['img']
-    filename = os.path.join(IMG_DIR, filename)
+    filename = 'data/lfw_funneled/Aaron_Eckhart/Aaron_Eckhart_0001.jpg'
     print(filename)
     img = cv.imread(filename)  # BGR
-    cv.imwrite('origin.png', img)
-    img = img[..., ::-1]  # RGB
-    img = Image.fromarray(img, 'RGB')  # RGB
-    img = transformer(img)
     img = image_aug(img)  # RGB
-    img = img[..., ::-1]  # BGR
-    cv.imwrite('out.png', img)
+    cv.imshow('', img)
+    cv.waitKey(0)
