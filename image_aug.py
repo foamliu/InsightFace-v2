3c2dc89574254f00c3d7c5ab3e17da96f4dc1cf0
Yang Liu <liuyang12@focusmedia.cn>
2019-09-24 15:12:24 +0800

    udpate

--- image_aug.py
+++ image_aug.py
@@ -30,6 +30,11 @@ if __name__ == "__main__":
     filename = 'data/lfw_funneled/Aaron_Eckhart/Aaron_Eckhart_0001.jpg'
     print(filename)
     img = cv.imread(filename)  # BGR
+    cv.imshow('', img)
+    cv.waitKey(0)
     img = image_aug(img)  # RGB
+    cv.imshow('', img)
+    cv.waitKey(0)
+    img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     cv.imshow('', img)
     cv.waitKey(0)
