5bb3284ad89be344c401fdf31bee2df646cb0924
Yang Liu <liuyang12@focusmedia.cn>
2019-09-24 15:16:05 +0800

    udpate

--- image_aug.py
+++ image_aug.py
@@ -2,6 +2,7 @@ import cv2 as cv
 import numpy as np
 from imgaug import augmenters as iaa
 from torchvision import transforms
+from utils import get_central_face_attributes, align_face
 
 # Data augmentation and normalization for training
 # Just normalization for validation
@@ -32,9 +33,16 @@ if __name__ == "__main__":
     img = cv.imread(filename)  # BGR
     cv.imshow('', img)
     cv.waitKey(0)
+
+    is_valid, bounding_boxes, landmarks = get_central_face_attributes(filename)
+    img = align_face(filename, landmarks)
+    cv.imshow('', img)
+    cv.waitKey(0)
+
     img = image_aug(img)  # RGB
     cv.imshow('', img)
     cv.waitKey(0)
+
     img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     cv.imshow('', img)
     cv.waitKey(0)
