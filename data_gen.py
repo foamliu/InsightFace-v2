15f86d3fc28a13088a4f52816e280740f1d17e07
Yang Liu <liuyang12@focusmedia.cn>
2019-10-28 14:34:37 +0800

    update

--- data_gen.py
+++ data_gen.py
@@ -1,19 +1,19 @@
 import os
 import pickle
 
-import cv2 as cv
 from PIL import Image
 from torch.utils.data import Dataset
 from torchvision import transforms
 
 from config import IMG_DIR
 from config import pickle_file
-from image_aug import image_aug
 
 # Data augmentation and normalization for training
 # Just normalization for validation
 data_transforms = {
     'train': transforms.Compose([
+        transforms.RandomHorizontalFlip(),
+        transforms.ColorJitter(brightness=0.125, contrast=0.125, saturation=0.125),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
     ]),
@@ -39,11 +39,8 @@ class ArcFaceDataset(Dataset):
         label = sample['label']
 
         filename = os.path.join(IMG_DIR, filename)
-        img = cv.imread(filename)  # BGR
-        img = img[..., ::-1]  # RGB
-        img = Image.fromarray(img, 'RGB')  # RGB
-        img = image_aug(img)  # RGB
-        img = self.transformer(img)  # RGB
+        img = Image.open(filename).convert('RGB')
+        img = self.transformer(img)
 
         return img, label
 
