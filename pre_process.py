e7fce718128f7c1f8e2f2aba2866d4d3a631621a
Yang Liu <liuyang12@focusmedia.cn>
2019-12-01 12:00:53 +0800

    update

--- pre_process.py
+++ pre_process.py
@@ -19,24 +19,24 @@ if __name__ == "__main__":
     # %% 1 ~ 3804847
     for i in tqdm(range(10000000)):
         # print(i)
-        # try:
-        header, s = recordio.unpack(imgrec.read_idx(i + 1))
-        img = mx.image.imdecode(s).asnumpy()
-        print(img.shape)
-        img = cv.cvtColor(img, cv.COLOR_RGB2BGR)
-        print(header.label)
-        print(type(header.label))
-        # label = int(header.label)
-        # filename = '{}.png'.format(i)
-        # samples.append({'img': filename, 'label': label})
-        # filename = os.path.join(IMG_DIR, filename)
-        # cv.imwrite(filename, img)
-        # except KeyboardInterrupt:
-        #     raise
-        # except Exception as err:
-        #     print(err)
-        #     print(i)
-        #     # pass
+        try:
+            header, s = recordio.unpack(imgrec.read_idx(i + 1))
+            img = mx.image.imdecode(s).asnumpy()
+            # print(img.shape)
+            img = cv.cvtColor(img, cv.COLOR_RGB2BGR)
+            # print(header.label)
+            # print(type(header.label))
+            label = int(header.label[0])
+            filename = '{}.png'.format(i)
+            samples.append({'img': filename, 'label': label})
+            filename = os.path.join(IMG_DIR, filename)
+            cv.imwrite(filename, img)
+        except KeyboardInterrupt:
+            raise
+        except Exception as err:
+            print(err)
+            print(i)
+            # pass
 
     with open(pickle_file, 'wb') as file:
         pickle.dump(samples, file)
